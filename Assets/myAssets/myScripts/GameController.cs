be45e8d20907f46554378cc358592045a6c41079
Zachary Kolibas <zachary_kolibas@bloomfield.edu>
2020-09-25 22:31:39 -0400

    Re-Add  9/25

---
+++ Assets/myAssets/myScripts/GameController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject birdPrefab;
+    public Transform birdSpawnLocation;
+    public GameObject playButton;
+    public ObstacleSpawner obstacleSpawnerRef;
+    public int points;
+    public TextMeshProUGUI scoreText;
+
+    bool isAlive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.SetActive(true);
+        obstacleSpawnerRef.enabled = false;
+    }
+
+    void Update()
+    {
+        if(birdPrefab == null)
+        {
+            EndGame();
+        }
+        
+    }
+
+    public void BeginGame()
+    {
+        if (birdPrefab != null)
+        {
+            SpawnBird();
+            obstacleSpawnerRef.enabled = true;
+            playButton.SetActive(false);
+            points = 0;
+        }
+    }
+
+    void EndGame()
+    {
+        SceneManager.LoadScene("FlappyBirdHome");
+    }
+
+    void SpawnBird()
+    {
+        Instantiate(birdPrefab,birdSpawnLocation.position, birdSpawnLocation.rotation);
+    }
+
+    public void AddPoints()
+    {
+        points++;
+        print("My Points: " + points);
+    }
+}
---
+++ Assets/myAssets/myScripts/ObstacleSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public float spawnDelay;
+    public GameObject ObstaclePrefab;
+    private Vector3 randomHeightVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnObstacle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        Vector3 randomHeightVector = new Vector3(10, Random.Range(-2f, 2f), 0);
+        Instantiate(ObstaclePrefab, randomHeightVector, Quaternion.identity);
+        Invoke("SpawnObstacle", spawnDelay);
+    }
+}
